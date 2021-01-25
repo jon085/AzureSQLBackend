aac6d6dd9055564ecbd9ef1613aca026ad5cb56b
JontyK <jontykarty@gmail.com>
2021-01-25 15:00:45 +0100

    Added a books table for example

---
+++ tables/books.js
@@ -0,0 +1,43 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+// Note: This has been adapted from the Azure Sample:
+// https://github.com/azure/azure-mobile-apps-node/tree/master/samples/todo
+
+var azureMobileApps = require('azure-mobile-apps');
+const accessRights = {anonymous: "anonymous", authenticated: "authenticated", disabled: "disabled"}; //Table Access Parameters
+
+// Create a table with this name
+azureMobileApps.tables.add('Books')
+
+// Create a new table definition
+var table = azureMobileApps.table();
+
+table.dynamicSchema = true;
+
+// Configure table options
+table.access = accessRights.anonymous;
+table.read.access = accessRights.anonymous;
+table.insert.access = accessRights.anonymous;
+table.update.access = accessRights.anonymous;
+table.delete.access = accessRights.anonymous;
+
+// Configure specific code when the client does a request
+table.read(function (context) {
+    return context.execute();
+});
+
+table.insert(function (context) {
+    return context.execute();
+});
+
+table.update(function (context) {
+    return context.execute();
+});
+
+table.delete(function (context) {
+    return context.execute();
+});
+
+module.exports = table;
