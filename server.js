3d52bda47f12d12f1482c011864e47bb1054b045
JontyK <jontykarty@gmail.com>
2021-01-26 12:53:50 +0100

    Adding missing TravelRecord Table to Server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobil
 
 mobileApp.tables.add('Book'); // Create a table for 'Book' with default settings
 mobileApp.tables.add('Users');// Create a table association
+mobileApp.tables.add('TravelRecord');// Create a table association
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
